15d8eac9bfd337839ffb836912c28ad6b1830146
Deepak Barwal <deepakbarwal98@gmail.com>
2024-08-10 13:31:02 +0530

    add: login page

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+type BackButtonType = {
+  href: string;
+  label: string;
+};
+
+export default function BackButton({ href, label }: BackButtonType) {
+  return (
+    <Button className="font-medium w-full">
+      <Link aria-label={label} href={href}>
+        {label}
+      </Link>
+    </Button>
+  );
+}
